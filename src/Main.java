0e08689ba237a8f8eb3e418725532f3ca01e44ae
sadie-boop <sadie.stribley@gmail.com>
2026-02-22 11:27:47 -0700

    updated restaurant

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class Main {
     static void main(String[] args) {
+        // Instantiate an ArrayList to hold Restaurant Object
+        ArrayList<Restaurant> restaurantsList = new ArrayList<>();
 
-        Restaurant r1 = new Restaurant();
-        Restaurant r2 = new Restaurant(30, 3.9, 17.89, 2.7, "6:00 am - 9:00pm");
+        // Take user input using JOptionPane and store in a variable
+        String restaurantName = JOptionPane.showInputDialog("Enter the Restaurant name, please:");
+        int waitTime = Integer.parseInt(JOptionPane.showInputDialog("How long was the Restaurants wait time in minutes Ex(30):"));
+        double rating = Double.parseDouble(JOptionPane.showInputDialog("What would you rate this Restaurant from 0.0 to 10.00"));
+        double avgPrice = Double.parseDouble(JOptionPane.showInputDialog("Enter average price of the restaurant Ex(12.50): "));
+        double distance = Double.parseDouble(JOptionPane.showInputDialog("Enter the distance the restaurant in Mile Ex(5.5): "));
+        String hoursOpen = JOptionPane.showInputDialog("Enter hours of operation: Ex(9:00am - 10:00pm");
 
-        System.out.println(r2.waitTime);
-        System.out.println(r2.rating);
-        System.out.println(r2.avgPrice);
-        System.out.println(r2.distance);
-        System.out.println(r2.hoursOpen);
+        // Instantiate Restaurant object with those variables
+
+        Restaurant userRestaurant = new Restaurant(restaurantName, waitTime, avgPrice, distance, hoursOpen);
+        System.out.println(userRestaurant.toString());
+
+        // Add user's restaurant object to the arraylist
+        restaurantsList.add(userRestaurant);
+
+
     }
 }
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,23 +1,81 @@
 public class Restaurant {
     // Properties
-    int waitTime;
-    double rating;
-    double avgPrice;
-    double distance;
-    String hoursOpen;
+    private String restaurantName;
+    private int waitTime;
+    private double rating;
+    private double avgPrice;
+    private double distance;
+    private String hoursOpen;
+
     /// menu items
 
     // Constructor
-    public Restaurant(){}
+    public Restaurant() {}
 
-    public Restaurant(int waitTime, double rating, double avgPrice, double distance,String hoursOpen){
+    public Restaurant(String restaurantName, int waitTime, double avgPrice, double distance, String hoursOpen) {
+        this.restaurantName = restaurantName;
         this.waitTime = waitTime;
-        this.rating = rating;
         this.avgPrice = avgPrice;
         this.distance = distance;
         this.hoursOpen = hoursOpen;
+
+
+    }
+    // Getters and setters
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public int getWaitTime() { return waitTime; }
+
+
+    public void setWaitTime(int waitTime) {this.waitTime = waitTime;}
+
+    public double getRating() {return rating;}
+
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getAvgPrice() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(double avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 
 
+    public String getHoursOpen() {
+        return hoursOpen;
+    }
 
+    public void setHoursOpen(String hoursOpen) {
+        this.hoursOpen = hoursOpen;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant Name:\t " + restaurantName +
+                "\nWait Time:\t\t\t" + waitTime +
+                "\nRating:\t\t\t\t" + rating +
+                "\nAverage Price:\t\t" + avgPrice +
+                "\nDistance:\t\t\t" + distance +
+                "\nHours of Operation:" + hoursOpen;
+    }
 }
+
+
