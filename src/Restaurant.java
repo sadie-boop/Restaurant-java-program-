c3dc7c9988f808410df707b28dc65b4c1c8e46ee
sadie-boop <sadie.stribley@gmail.com>
2026-03-25 20:43:31 -0600

    Refactor Restaurant class properties and methods

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,81 +1,50 @@
 public class Restaurant {
-    // Properties
-    private String restaurantName;
+    private String name;
     private int waitTime;
     private double rating;
     private double avgPrice;
     private double distance;
     private String hoursOpen;
 
-    /// menu items
+    public Restaurant() {
+    }
 
-    // Constructor
-    public Restaurant() {}
-
-    public Restaurant(String restaurantName, int waitTime, double avgPrice, double distance, String hoursOpen) {
-        this.restaurantName = restaurantName;
+    public Restaurant(String name, int waitTime, double rating, double avgPrice, double distance, String hoursOpen) {
+        this.name = name;
         this.waitTime = waitTime;
+        this.rating = rating;
         this.avgPrice = avgPrice;
         this.distance = distance;
         this.hoursOpen = hoursOpen;
-
-
     }
-    // Getters and setters
 
-    public String getRestaurantName() {
-        return restaurantName;
-    }
-    public void setRestaurantName(String restaurantName) {
-        this.restaurantName = restaurantName;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
     public int getWaitTime() { return waitTime; }
+    public void setWaitTime(int waitTime) { this.waitTime = waitTime; }
 
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
 
-    public void setWaitTime(int waitTime) {this.waitTime = waitTime;}
+    public double getAvgPrice() { return avgPrice; }
+    public void setAvgPrice(double avgPrice) { this.avgPrice = avgPrice; }
 
-    public double getRating() {return rating;}
+    public double getDistance() { return distance; }
+    public void setDistance(double distance) { this.distance = distance; }
 
+    public String getHoursOpen() { return hoursOpen; }
+    public void setHoursOpen(String hoursOpen) { this.hoursOpen = hoursOpen; }
 
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public double getAvgPrice() {
-        return avgPrice;
-    }
-
-    public void setAvgPrice(double avgPrice) {
-        this.avgPrice = avgPrice;
-    }
-
-    public double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(double distance) {
-        this.distance = distance;
-    }
-
-
-    public String getHoursOpen() {
-        return hoursOpen;
-    }
-
-    public void setHoursOpen(String hoursOpen) {
-        this.hoursOpen = hoursOpen;
-    }
-
-    @Override
     public String toString() {
-        return "Restaurant Name:\t " + restaurantName +
-                "\nWait Time:\t\t\t" + waitTime +
-                "\nRating:\t\t\t\t" + rating +
-                "\nAverage Price:\t\t" + avgPrice +
-                "\nDistance:\t\t\t" + distance +
-                "\nHours of Operation:" + hoursOpen;
+        String col1 = "%-17s ";
+        String nameFormat   = String.format(col1 + "%-30s%n", "Name:", name);
+        String waitFormat   = String.format(col1 + "%-5d%n",  "Wait Time:", waitTime);
+        String ratingFormat = String.format(col1 + "%-5.2f%n","Rating:", rating);
+        String avgFormat    = String.format(col1 + "%-5.2f%n","Average Price:", avgPrice);
+        String distFormat   = String.format(col1 + "%-5.2f%n","Distance:", distance);
+        String hoursFormat  = String.format(col1 + "%-30s%n", "Open:", hoursOpen);
+
+        return nameFormat + waitFormat + ratingFormat + avgFormat + distFormat + hoursFormat;
     }
 }
-
-
